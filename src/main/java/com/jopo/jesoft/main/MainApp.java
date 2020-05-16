6b9c5d55a3dde8a6b55a67e44f4fc8a4645526e1
joel <joel.huaman.h@gmail.com>
2020-05-15 20:45:35 -0500

    init commit

---
+++ src/main/java/com/jopo/jesoft/main/MainApp.java
@@ -0,0 +1,44 @@
+package com.jopo.jesoft.main;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+   
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            System.out.println("Ingrese al metodo START");
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Marcas.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
+        } catch (Exception e) {
+            System.out.println("Error"+e);
+        }
+        
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
